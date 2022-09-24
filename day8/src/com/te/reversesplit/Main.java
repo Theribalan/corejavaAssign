634c2214e5a7e58ce8c936a3ef656bfa4f3d46d3
Theribalan <balulingam007@gmail.com>
2022-09-25 00:14:00 +0530

    day8

---
+++ day8/src/com/te/reversesplit/Main.java
@@ -0,0 +1,16 @@
+package com.te.reversesplit;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter your string value :: ");
+		String string1 = scanner.nextLine();
+		System.out.println("enter your character:: ");
+		char char1 = scanner.next().charAt(0);
+		System.out.println(UserMainCode.reshape(string1, char1));
+		scanner.close();
+	}
+
+}
---
+++ day8/src/com/te/reversesplit/UserMainCode.java
@@ -0,0 +1,12 @@
+package com.te.reversesplit;
+
+public class UserMainCode {
+	public static String reshape(String s1, char ch) {
+		String s2 = "";
+		for (int i = s1.length() - 1; i >= 0; i--) {
+			s2 = s2 + s1.charAt(i) + ch;
+		}
+		return s2;
+	}
+
+}
